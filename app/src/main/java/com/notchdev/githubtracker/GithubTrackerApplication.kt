599cf73ccbfb05da181e30664f0a0d162357be45
ksog66 <ksog66@gmail.com>
2021-11-07 18:25:11 +0530

    initial commit

---
+++ app/src/main/java/com/notchdev/githubtracker/GithubTrackerApplication.kt
@@ -0,0 +1,8 @@
+package com.notchdev.githubtracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class GithubTrackerApplication: Application()
---
+++ app/src/main/java/com/notchdev/githubtracker/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.notchdev.githubtracker.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.notchdev.githubtracker.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
